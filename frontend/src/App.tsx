ee09e60de9b21e5bf2d7dafc2ec6442866c67047
Luke McCallum <luke.mccallum122@gmail.com>
2022-05-11 01:40:18 +1200

    cleared frontend template and set up pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '.Login.css';
+
+function Login() {
+    return <h1>Login</h1>;
+}
+
+export default Login;
