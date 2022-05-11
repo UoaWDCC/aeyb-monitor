540b88ba93b086ffd934779f215f19b57ef9dde3
pumbas600 <joshjeffers600@gmail.com>
2022-05-11 22:04:14 +1200

    Add express async handler dependency
    This automatically catches errors and passes it to the error middleware

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,15 +1,16 @@
+import asyncHandler from 'express-async-handler';
 import { Request, Response } from 'express';
 
 /**
  * @desc 	Get all the users
  * @route 	GET /api/users/
  */
-const getUsers = async (req: Request, res: Response) => {
+const getUsers = asyncHandler(async (req: Request, res: Response) => {
     // Fetch users
 
     res.status(200).json({
         message: 'Nice work, you just made a GET request!',
     });
-};
+});
 
 export { getUsers };
