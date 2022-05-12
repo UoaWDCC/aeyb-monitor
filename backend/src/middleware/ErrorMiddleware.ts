5c93d7c0041d9688fdc5e097a64ff2c0967593b2
pumbas600 <joshjeffers600@gmail.com>
2022-05-12 16:05:42 +1200

    Handle mongoose validation errors

--- backend/src/middleware/ErrorMiddleware.ts
+++ backend/src/middleware/ErrorMiddleware.ts
@@ -1,16 +1,34 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import ValidationError from '../types/ValidationError';
 
 function ErrorHandler(
-    err: Error,
+    err: Error | mongoose.Error.ValidationError,
     req: Request,
     res: Response,
-    next: NextFunction,
 ) {
+    // Check if the error was thrown due to invalid inputs for a model
+    if (err instanceof mongoose.Error.ValidationError) {
+        res.status(400).json({
+            message: 'There was something wrong with your request',
+            errors: getValidationErrors(err),
+        });
+        return;
+    }
+
     const status = res.statusCode ?? 501;
+    res.status(status).json({ message: err.message });
+}
 
-    // TODO: Add handling for Mongoose verification errors
-
-    res.status(status).json({ error: err.message });
+function getValidationErrors(
+    error: mongoose.Error.ValidationError,
+): ValidationError[] {
+    return Object.values(error.errors).map((err) => {
+        return {
+            field: err.path,
+            message: err.message,
+        };
+    });
 }
 
 export default ErrorHandler;
