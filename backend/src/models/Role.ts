4caca398011049cdf6ae5c38229e6950d412b23d
Lpar105 <Lpar105@aucklanduni.ac.nz>
2022-05-12 13:25:04 +1200

    adds Adds Role model and addRole

---
+++ backend/src/models/Role.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const roleSchema = new mongoose.Schema({
+    name:{
+        type: 'string',
+        required: true,
+        unique: true,
+        trim: true,
+        maxLength: 32,
+
+    },
+    color:{
+        type: String,
+        validate: {
+            validator: function(s:string) {
+                return /^#[0-9A-F]{6}$/i.test(s)
+            }
+        },
+        default: '#ffffff'
+    }, 
+    permissions: [String]
+})
+
+
+const Role = mongoose.model('Role', roleSchema);
+
+export default Role;
