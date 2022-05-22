7b921bedf37c41811f5984f45b94bc67111ecc5e
Lpar105 <Lpar105@aucklanduni.ac.nz>
2022-05-23 11:12:01 +1200

    adds general tidy ups

--- backend/src/routes/RoleRoutes.ts
+++ backend/src/routes/RoleRoutes.ts
@@ -4,7 +4,7 @@ import Permission from '../types/Perm';
 
 import { getAllRoles, getRole, deleteRole, addRole, updateRole } from '../controllers/RoleController';
 
-const RoleRouter = Router({ mergeParams: true });
+const RoleRouter = Router();
 
 RoleRouter.route('/').get(protect(Permission.VIEW_ROLES), getAllRoles).post(protect(Permission.ADD_ROLES), addRole);
 RoleRouter.route('/:roleId')
