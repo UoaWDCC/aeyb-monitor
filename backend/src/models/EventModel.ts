c6b17f212e5ab967f79035c632f29c38c54cf4d3
pumbas600 <joshjeffers600@gmail.com>
2022-06-04 23:54:53 +1200

    Create EventModel

---
+++ backend/src/models/EventModel.ts
@@ -0,0 +1,42 @@
+import mongoose, { model, Schema } from 'mongoose';
+import { UserModel } from './UserModel';
+
+export interface EventModel {
+    _id: mongoose.Types.ObjectId;
+    name: string;
+    creator: UserModel;
+    time: Date;
+    location: string;
+    description?: string;
+}
+
+const eventSchema = new Schema<EventModel>({
+    _id: {
+        type: Schema.Types.ObjectId,
+        required: [true, "You must specify the event's id"],
+    },
+    name: {
+        type: String,
+        required: [true, 'You must specify the name of the event'],
+        trim: true,
+    },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'You must specify the user who created the event'],
+    },
+    time: {
+        type: Date,
+        required: [true, 'You must specify when the event starts'],
+    },
+    location: {
+        type: String,
+        required: [true, 'You must specify where the event takes place'],
+        trim: true,
+    },
+    description: String,
+});
+
+const Event = model('event', eventSchema);
+
+export default Event;
