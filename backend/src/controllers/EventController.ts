dd1c0d46de2c46b1daeef084961b0e45072466a2
pumbas600 <joshjeffers600@gmail.com>
2022-06-25 21:11:12 +1200

    Add CRUD endpoints for events

--- backend/src/controllers/EventController.ts
+++ backend/src/controllers/EventController.ts
@@ -1,8 +1,9 @@
 import asyncHandler from 'express-async-handler';
 import { Request, Response } from 'express';
-import Event from '../models/EventModel';
+import Event, { EventModel } from '../models/EventModel';
 import { EventIdParam } from '../types/RequestParams';
-import Role from '../models/RoleModel';
+import { TypedRequest, TypedRequestBody } from '../types/UtilTypes';
+import { RoleModel } from '../models/RoleModel';
 
 /**
  * @desc    Get all the events
@@ -42,4 +43,53 @@ const getEvent = asyncHandler(async (req: Request<EventIdParam>, res: Response) 
     });
 });
 
-export { getAllEvents, getEvent };
+/**
+ * @desc    Add a new event
+ * @route   POST /api/events/
+ */
+const addEvent = asyncHandler(async (req: TypedRequestBody<EventModel>, res: Response) => {
+    const newEvent = await Event.create(req.body);
+
+    await res.status(201).json({
+        status: 'success',
+        data: {
+            event: newEvent,
+        },
+    });
+});
+
+/**
+ * @desc    Delete a specific event
+ * @route   DELETE /api/events/:eventId
+ */
+const deleteEvent = asyncHandler(async (req: Request<EventIdParam>, res: Response) => {
+    const response = await Event.findByIdAndDelete(req.params.eventId);
+    if (response) {
+        res.status(204);
+        return;
+    }
+    res.status(404).json({
+        status: 'error',
+        message: `There is no event with the id ${req.params.eventId}`,
+    });
+});
+
+/**
+ * @desc    Edit a specific event
+ * @route   PATCH /api/events/:eventId
+ */
+const updateEvent = asyncHandler(async (req: TypedRequest<EventModel, EventIdParam>, res: Response) => {
+    const event = await Event.findByIdAndUpdate(req.params.eventId, req.body, {
+        new: true,
+        runValidators: true,
+    });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            event,
+        },
+    });
+});
+
+export { getAllEvents, getEvent, addEvent, deleteEvent, updateEvent };
