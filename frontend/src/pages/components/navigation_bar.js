22cbeb5fcaede89b3b171cbb0719c7f2daf02cb7
LeeVioletOng <leevioletong@gmail.com>
2022-07-01 23:52:10 +1200

    Responsive and Extending Nav Bar

--- frontend/src/pages/components/navigation_bar.js
+++ frontend/src/pages/components/navigation_bar.js
@@ -1,14 +1,24 @@
-import { ReactElement } from 'react';
 import IonIcon from '@reacticons/ionicons'; 
 import img from "../../images/logos/IG_story1.jpg";
+import React, { useState } from 'react';
 
 const NavBar = () =>{
+    const [open,setOpen] = useState(false);
     return( 
-        <div className = "absolute top-0 left-0 min-h-screen w-32 m-0 flex flex-col text-blue-800 font-bold items-center space-y-56 shadow-lg">
-            <img src={img}></img>
-            <button><IonIcon name="calendar-clear" size='large' className='hover:text-blue-500'/></button>
-            <button><IonIcon name="home" size='large' className='hover:text-blue-500'/></button>
-            <button><IonIcon name="person" size='large' className='hover:text-blue-500'/></button>
+        <div className = {`absolute md:top-0 left-0 md:min-h-screen ${open? "md:w-72":"md:w-32"} duration-300 m-0 flex md:flex-col sm:bottom-0 sm:inset-x-0 sm:min-w-screen sm:h-32 sm:space-x-24 text-blue-800 font-bold md:space-y-56 md:space-x-12 shadow-lg`}>
+            <img src={img} className='hidden w-32 md:block cursor-pointer' onClick={()=>setOpen(!open)}></img>
+            <button className='flex gap-x-12 hover:text-blue-500'>
+                <div><IonIcon name="calendar-clear" size='large'/></div>
+                <span className={`hidden ${open? "md:block":"md:hidden"} text-2xl`}>CALENDAR</span>
+            </button>
+            <button className='flex gap-x-12 hover:text-blue-500'>
+                <div><IonIcon name="home" size='large'/></div>
+                <span className={`hidden ${open? "md:block":"md:hidden"} text-2xl`}>HOME</span>
+            </button>
+            <button className='flex gap-x-12 hover:text-blue-500'>
+                <div><IonIcon name="person" size='large'/></div>
+                <span className={`hidden ${open? "md:block":"md:hidden"} text-2xl`}>PROFILE</span>
+            </button>
         </div>
     );
 }
