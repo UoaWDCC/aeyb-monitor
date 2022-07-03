423e1e0ae0972148b48c8f9c5f3a44a4308b9c73
LeeVioletOng <leevioletong@gmail.com>
2022-07-04 11:47:11 +1200

    Refactoring and implementation of new design

---
+++ frontend/src/pages/components/notification_bar.js
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react';
+import IonIcon from '@reacticons/ionicons'; 
+
+const NotifBar = () =>{
+    return(
+        <div className='text-red-900'>
+            <p>test</p>
+        </div>
+    );
+}
+
+export default NotifBar;
--- frontend/src/pages/profiles/ProfileInfo.tsx
+++ frontend/src/pages/profiles/ProfileInfo.tsx
@@ -1,7 +1,8 @@
 import { ReactElement } from 'react';
 import { Profile, UserStatus, Roles } from './Interface';
+import NavBar from '../components/navigation_bar.js';
+import NotifBar from '../components/notification_bar.js';
 import React, { useState } from 'react';
-import IonIcon from '@reacticons/ionicons';
 
 interface ProfileInformation {
     profile: Profile;
@@ -15,71 +16,9 @@ const ProfileInfo = (props: ProfileInformation): ReactElement => {
 
     return (
         <div>
-            <div id="title">
-                <h1>Profile</h1>
-            </div>
-            <div id="mainbody">
-                <nav>
-                    <div id="navi">
-                        <button>
-                            <span className="icon">
-                                <IonIcon name="home" />
-                                <h4>HOME</h4>
-                            </span>
-                        </button>
-                        <button>
-                            <span className="icon">
-                                <IonIcon name="calendar-clear" />
-                                <h4>CALENDER</h4>
-                            </span>
-                        </button>
-                        <button>
-                            <span className="icon">
-                                <IonIcon name="heart" />
-                                <h4>STATS</h4>
-                            </span>
-                        </button>
-                    </div>
-                </nav>
-                <div id="profile">
-                    <button id="editButton" onClick={() => setEdit(!editClick)}>
-                        <span className="icon">
-                            <IonIcon name="create-outline" />
-                        </span>
-                    </button>
-                    <div
-                        id="edit"
-                        style={{ display: editClick ? 'block' : 'none' }}
-                    >
-                        <button onClick={() => setStatus(UserStatus.Available)}>
-                            available
-                        </button>
-                        <button onClick={() => setStatus(UserStatus.Busy)}>
-                            busy
-                        </button>
-                        <button onClick={() => setStatus(UserStatus.Idle)}>
-                            idle
-                        </button>
-                    </div>
-                    <div id="imageBox">
-                        {' '}
-                        <img src={props.profile.profilePictureUrl}></img>
-                        <span
-                            id="status"
-                            style={{ backgroundColor: status }}
-                        ></span>
-                    </div>
-
-                    <h2>{props.profile.name}</h2>
-                    <div id="roles">
-                        {props.profile.roles.map((role) => {
-                            return <span>{role}</span>;
-                        })}
-                    </div>
-                </div>
-                <div id="settings">
-                    <button>Role Permissions</button>
-                </div>
+            <div className="mainbody">
+                <NavBar/> 
+                
             </div>
         </div>
     );
