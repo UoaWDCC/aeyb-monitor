f2e9735eacf6143bd1f03b436918f04d4df43efb
rish-shuk <rish.shuk@gmail.com>
2022-07-03 22:52:06 +1200

    To Implement clientID

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,30 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { idText } from 'typescript';
 import './Login.css';
 
 function Login() {
-    return <h1>test</h1>;
+    const onFailure = (result) => {
+        alert(result);
+    };
+    const onSuccess = (googleData) => {
+        console.log(googleData);
+    };
+    return (
+        <div className="loginPage">
+            <header className="loginPageHeader">
+                <h1>Welcome to the AEYB Portal</h1>
+                <GoogleLogin
+                    //need id
+                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                    buttonText="Login"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                ></GoogleLogin>
+            </header>
+        </div>
+    );
 }
 
 export default Login;
