236031c0b6aea8d1c1e6bd6d74c8182b889cd920
pumbas600 <joshjeffers600@gmail.com>
2022-07-04 15:51:02 +1200

    Check that event exists when trying to update it

--- backend/src/routes/EventRoutes.ts
+++ backend/src/routes/EventRoutes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { addEvent, deleteEvent, getAllEvents, getEvent, updateEvent } from '../controllers/EventController';
 import protect from '../middleware/AuthMiddleware';
-import Permission from '../types/Perm';
 
 const EventRouter = Router();
 
