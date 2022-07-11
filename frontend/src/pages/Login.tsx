713527c2a2e4fc87361494795b558f36649f9e7a
Luke McCallum <luke.mccallum122@gmail.com>
2022-07-11 18:58:31 +1200

    implementation of draft design

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
 import './Login.css';
 
@@ -12,14 +11,16 @@ function Login() {
     return (
         <div className="loginPage">
             <header className="loginPageHeader">
-                <h1>Welcome to the AEYB Portal</h1>
+                <h1>Welcome to AEYB</h1>
                 <GoogleLogin
+                    className="loginBox"
                     clientId="931818604859-0jd0r03np411c0v0pp89daplg1eansep.apps.googleusercontent.com"
-                    buttonText="Login"
                     onSuccess={onSuccess}
                     onFailure={onFailure}
                     cookiePolicy={'single_host_origin'}
-                />
+                >
+                    Sign in with Google
+                </GoogleLogin>
             </header>
         </div>
     );
