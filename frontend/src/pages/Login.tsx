329ef2ff4b85ecc10ddfe6bb01c6077fa1cd3eeb
luke-mccallum <luke.mccallum122@gmail.com>
2022-08-02 16:33:29 +1200

    googleData type declaration

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import GoogleLogin from 'react-google-login';
+import GoogleLogin, { GoogleLoginResponse } from 'react-google-login';
 import './Login.css';
 import axios from 'axios';
 
@@ -8,7 +8,7 @@ function Login() {
         console.log('Invalid account');
     };
 
-    const onSuccess = (googleData) => {
+    const onSuccess = (googleData: GoogleLoginResponse) => {
         axios
             .post('http://localhost:5000/api/users/login', {
                 credential: googleData.tokenId,
@@ -25,7 +25,7 @@ function Login() {
                 <GoogleLogin
                     className="loginBox"
                     clientId="931818604859-0jd0r03np411c0v0pp89daplg1eansep.apps.googleusercontent.com"
-                    onSuccess={onSuccess}
+                    onSuccess={(res) => onSuccess(res as GoogleLoginResponse)}
                     onFailure={onFailure}
                     cookiePolicy={'single_host_origin'}
                     isSignedIn={true}
