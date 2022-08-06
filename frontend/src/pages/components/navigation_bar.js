bf5beb7c15fd2a29b14c007df16916356541780a
long715 <long715@aucklanduni.ac.nz>
2022-08-07 01:01:36 +1200

    Profile Information and Settings implemented as per current HIFI design

--- frontend/src/pages/components/navigation_bar.js
+++ frontend/src/pages/components/navigation_bar.js
@@ -1,12 +1,12 @@
 import IonIcon from '@reacticons/ionicons'; 
-import img from "../../images/logos/IG_story1.jpg";
+import img from "../../images/logos/AEYB_A0_Circle.png";
 import React, { useState } from 'react';
 
 const NavBar = () =>{
     const [open,setOpen] = useState(false);
     return( 
-        <div className = {`absolute md:top-0 left-0 md:min-h-screen ${open? "md:w-72":"md:w-32"} duration-300 m-0 flex md:flex-col sm:bottom-0 sm:inset-x-0 sm:min-w-screen sm:h-32 sm:space-x-24 text-blue-800 font-bold md:space-y-56 md:space-x-12 shadow-lg`}>
-            <img src={img} className='hidden w-32 md:block cursor-pointer' onClick={()=>setOpen(!open)}></img>
+        <div className = {`absolute md:top-0 left-0 md:h-screen ${open? "md:w-72":"md:w-32"} duration-300 m-0 flex md:flex-col sm:bottom-0 sm:inset-x-0 sm:min-w-screen sm:h-32 sm:space-x-24 text-blue-800 font-bold md:space-y-40 md:space-x-12 shadow-lg bg-white`}>
+            <img src={img} className='hidden w-32 mt-8 md:block cursor-pointer' onClick={()=>setOpen(!open)}></img>
             <button className='flex gap-x-12 hover:text-blue-500'>
                 <div><IonIcon name="calendar-clear" size='large'/></div>
                 <span className={`hidden ${open? "md:block":"md:hidden"} text-2xl`}>CALENDAR</span>
--- frontend/src/pages/profiles/ProfileInfo.tsx
+++ frontend/src/pages/profiles/ProfileInfo.tsx
@@ -1,3 +1,5 @@
+import IonIcon from '@reacticons/ionicons'; 
+
 import { ReactElement } from 'react';
 import { Profile, UserStatus, Roles } from './Interface';
 import NavBar from '../components/navigation_bar.js';
@@ -9,17 +11,46 @@ interface ProfileInformation {
 }
 
 const ProfileInfo = (props: ProfileInformation): ReactElement => {
-    // updates the status during onclick events
-    const [status, setStatus] = React.useState<UserStatus>(UserStatus.Idle);
-    // makes the nsub navigation for setStatus visibile during an on click event
-    const [editClick, setEdit] = React.useState(false);
 
     return (
         <div>
-            <div className="mainbody">
-                <NavBar/> 
-                <NotifBar/>
+            <div className='flex flex-col items-center w-screem h-screen'>
+                {/*Basic Information Div
+                    - one role shown which is their main role 
+                    - main role should always be the first role given to the user 
+                      OR group the roles into categories 
+                 */}
+                <div className='w-1/3 flex flex-row justify-center mt-9'>
+                    <div>
+                        <img className='rounded-full scale-90' src={props.profile.profilePictureUrl}/>
+                    </div>
+                    <div className='mt-10 ml-10 space-y-8 text-4xl'>
+                        <p className='font-bold mb-3 text-[#262B6C]'>{props.profile.name}</p>
+                        <span className=' border-[#FF0201] text-[#FF0201] border-2 rounded-md px-4'>{props.profile.roles[0]}</span>
+                    </div>
+                </div>
+
+                {/*Settings Div 
+                    - output will depend on main role
+                */}
+                <div className='flex flex-col w-1/3 mt-10 text-4xl text-[#262B6C]'>
+                    <button className='border-solid border-t border-[#262B6C] text-left flex-row inline-flex px-8'>
+                        <p className='py-16 w-3/4 h-full'>Personal Stats</p>
+                        <div className='py-16 w-1/4 h-full text-right'><IonIcon name="chevron-forward-outline"/></div>
+                    </button>
+                    <button className='border-solid border-t border-[#262B6C] text-left flex-row inline-flex px-8'>
+                        <p className='py-16 w-3/4 h-full'>Meeting Stats</p>
+                        <div className='py-16 w-1/4 h-full text-right'><IonIcon name="chevron-forward-outline"/></div>
+                    </button>
+                    <button className='border-solid border-t border-[#262B6C] text-left flex-row inline-flex px-8'>
+                        <p className='py-16 w-3/4 h-full'>Roles</p>
+                        <div className='py-16 w-1/4 h-full text-right'><IonIcon name="chevron-forward-outline"/></div>
+                    </button>
+                </div>
             </div>
+
+            <NavBar/> 
+            <NotifBar/>
         </div>
     );
 };
