03170a7561ae1571d93ea457289c29be0951c153
pumbas600 <joshjeffers600@gmail.com>
2022-08-07 17:17:31 +1200

    #28 Create response utility methods

--- backend/src/types/ResponseAugmentation.ts
+++ backend/src/types/ResponseAugmentation.ts
@@ -7,7 +7,29 @@ import { response } from 'express';
 declare module 'express-serve-static-core' {
     export interface Response {
         invalid(message: string): void;
+        unauthorized(message: string): void;
+        ok<T>(data: T): void;
     }
 }
 
-response.invalid = (message) => {};
+// For some reason 'this' is undefined if I try and use an arrow functions.
+response.invalid = function (message) {
+    this.status(400).json({
+        status: 'error',
+        message,
+    });
+};
+
+response.unauthorized = function (message) {
+    this.status(404).json({
+        status: 'unauthorized',
+        message,
+    });
+};
+
+response.ok = function (data) {
+    this.status(200).json({
+        status: 'success',
+        data,
+    });
+};
