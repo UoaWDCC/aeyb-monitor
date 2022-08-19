a73944a21ffd0e2286b07d52b14c084fee63836b
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-08-19 15:01:55 +1200

    div placement for the meetings complete

--- frontend/src/pages/Hompage/Homepage.tsx
+++ frontend/src/pages/Hompage/Homepage.tsx
@@ -16,8 +16,26 @@ export default function Homepage() {
       <Sidebar isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} currentPage={0} />
 
       <div className='pageComponent'>
-        {/* There would be components present for homepage */}
-        <p>This is the Home Page</p>
+        <div className="containerAll">
+          <div id="liveContainer">
+            <div className="live">LIVEMEETINGPLACEHOLDER</div>
+            <div className="live">LIVEMEETINGPLACEHOLDER</div>
+          </div>
+          <div id='meetingContainer'>
+            <div id="upcomingContainer" className='mContainer'>
+              <p className='containerTtl'>Upcoming meetings:</p>
+              <div className="upcoming">UPCOMINGPLACEHOLDER</div>
+              <div className="upcoming">UPCOMINGPLACEHOLDER</div>
+              <div className="upcoming">UPCOMINGPLACEHOLDER</div>
+            </div>
+            <div id="postmeetingContainer" className='mContainer'>
+              <p className='containerTtl'>post meetings:</p>
+              <div className="postMeeting">POSTMEETINGPLACEHOLDER</div>
+              <div className="postMeeting">POSTMEETINGPLACEHOLDER</div>
+              <div className="postMeeting">POSTMEETINGPLACEHOLDER</div>
+            </div>
+          </div>
+        </div>
       </div>
 
     </ >
