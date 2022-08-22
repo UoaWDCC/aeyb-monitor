d5ad8e6e08cbf6cf1135e5aed150db9082384a37
Grant Liu <grant.liux@gmail.com>
2022-08-22 21:07:25 +1200

    Added page heading

--- frontend/src/pages/Roles/Roles.tsx
+++ frontend/src/pages/Roles/Roles.tsx
@@ -11,19 +11,24 @@ function Roles() {
     const allRoles = ['Admin', 'User', 'Guest', 'TestRole1', 'TestRole2'];
 
     return (
-        <div className="bg-[#bdc3e3] w-full md:pl-[90px] p-4 h-screen rounded-md md:grid md:grid-cols-3 md:gap-12 overflow-scroll">
-            <div className="flex flex-col">
-                <div className="h-[300px]">
-                    <RoleList allRoles={allRoles} />
-                </div>
-
-                <div className="h-[300px]">
-                    <UserList allUsers={allUsers} />
-                </div>
+        <div className=" md:pl-[90px] bg-[#bdc3e3] overflow-scroll h-screen">
+            <div className="px-4 pt-2">
+                <h1 className="text-5xl text-[#262b6c]">Permissions</h1>
             </div>
+            <div className=" w-full p-4  rounded-md md:grid md:grid-cols-3 md:gap-12 overflow-scroll">
+                <div className="flex flex-col">
+                    <div className="h-[300px]">
+                        <RoleList allRoles={allRoles} />
+                    </div>
 
-            <div className="bg-[#262b6c] col-span-2 p-2 rounded-md mt-10 md:mt-0 h-fit">
-                <PermissionList />
+                    <div className="h-[300px]">
+                        <UserList allUsers={allUsers} />
+                    </div>
+                </div>
+
+                <div className="bg-[#262b6c] col-span-2 p-2 rounded-md mt-10 md:mt-0 h-fit">
+                    <PermissionList />
+                </div>
             </div>
         </div>
     );
