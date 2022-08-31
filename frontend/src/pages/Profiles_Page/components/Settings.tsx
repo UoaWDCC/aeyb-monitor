51c2d52c4f69278875178375a2559215d855e0d3
long715 <long715@aucklanduni.ac.nz>
2022-08-31 14:20:58 +1200

    fixed font size for galaxy fold

--- frontend/src/pages/Profiles_Page/components/Settings.tsx
+++ frontend/src/pages/Profiles_Page/components/Settings.tsx
@@ -11,7 +11,7 @@ const SettingsBar = () => {
     };
 
     return (
-        <div className='flex flex-col w-3/4 md:w-3/5 mt-[5%] text-4xl text-[#262B6C]'>
+        <div className='flex flex-col w-3/4 md:w-3/5 mt-[5%] text-3xl text-[#262B6C]'>
             <button className='border-solid border-t border-[#262B6C] text-left flex-row inline-flex px-8'>
                 <p className='py-16 w-3/4 h-full'>Personal Stats</p>
                 <div className='py-16 w-1/4 h-full text-right'><IonIcon name="chevron-forward-outline" /></div>
