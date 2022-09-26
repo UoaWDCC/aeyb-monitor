6045750916c4d147023cac59e8f9c4c0c29918cf
Grant Liu <grant.liux@gmail.com>
2022-09-26 14:47:36 +1300

    Implmented searchable userList and MUI styled rating

--- frontend/src/pages/Active_Meeting/components/UserList.tsx
+++ frontend/src/pages/Active_Meeting/components/UserList.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 export default function UserList(props) {
     const { allUsers, setActiveUser } = props;
 
+    const [showUsers, setShowUsers] = React.useState(false);
+
     function handleUserSearch(search) {
         if (search === '') {
             setUsers(allUsers);
+            setShowUsers(false);
+            console.log("blank");
+
         } else {
+            setShowUsers(true);
             setUsers(allUsers.filter((user) => user.toLowerCase().includes(search.toLowerCase())));
         }
     }
@@ -14,13 +20,16 @@ export default function UserList(props) {
     //Users to display in the list
     const [users, setUsers] = React.useState(allUsers);
 
+
     return (
-        <div className="bg-[#5563ae] p-2 rounded-md h-full mt-4  border-2 border-[#262b6c]">
+        <div className="p-2 rounded-md h-full mt-4 relative">
             <h1 className="text-3xl text-white">Users</h1>
+
 
             {/* Search box */}
             <input
-                className="text bg-white w-full rounded-sm border-none outline-0 p-1"
+
+                className="text bg-gray-200 w-full rounded-sm border-none outline-0 p-1 m-0 mt-2"
                 type="text"
                 placeholder="Search for a User"
                 onChange={(prop: any) => {
@@ -28,15 +37,16 @@ export default function UserList(props) {
                 }}
             />
 
-            <div className="overflow-scroll h-3/4">
+            <div className={"absolute top-5/6 overflow-scroll w-full " + (showUsers ? 'visible' : 'invisible')}>
                 {/* Displays each user that has been searched for */}
                 {users.map((user) => {
                     return (
                         <div
-                            className="text-lg p-2 text-[#262b6c] bg-[#bdc3e3] mt-1 hover:text-[#bdc3e3] hover:bg-[#262b6c]"
+                            className="text-lg p-2 w-full m-0 text-[#262b6c] bg-[#bdc3e3] mt-1 hover:text-[#bdc3e3] hover:bg-[#262b6c]"
                             key={user}
                             onClick={() => {
                                 setActiveUser(user);
+                                setShowUsers(false);
                             }}
                         >
                             <p>{user}</p>
@@ -44,6 +54,6 @@ export default function UserList(props) {
                     );
                 })}
             </div>
-        </div>
+        </div >
     );
 }
