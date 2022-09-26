f3743f5513fdc8beb16d96061555108c0b0d22bd
Grant Liu <grant.liux@gmail.com>
2022-09-26 14:47:36 +1300

    Implmented searchable userList and MUI styled rating

--- frontend/src/pages/Active_Meeting/ActiveMeeting.tsx
+++ frontend/src/pages/Active_Meeting/ActiveMeeting.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Sidebar from '../Sidebar_Components/Sidebar'
 import UserList from './components/UserList';
+import Rating from './components/RadioGroupRating'
 
 export default function ActiveMeeting() {
     const allUsers = ['Hillary', 'Grant', 'Violet', 'Lauren', 'Luke', 'Sarah', 'Helen', 'Josh', 'Tyler'];
@@ -15,11 +16,24 @@ export default function ActiveMeeting() {
         setIsMenuOpen(!isMenuOpen)
     }
     return (
-        <div className='h-screen'>
+        <div className='h-screen overflow-hidden'>
             <Sidebar isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} currentPage={0} />
-            <div className='w-1/2  mx-auto my-10'>
+            <div className='w-full sm:w-1/2 h-32 mx-auto my-10 flex items-center flex-col'>
                 <h1 className='text-4xl'>Meeting A - with Group B</h1>
-                <UserList allUsers={allUsers} setActiveUser={setActiveUser} />
+                <div className='w-1/2'>
+                    <UserList allUsers={allUsers} setActiveUser={setActiveUser} />
+                </div>
+
+                {
+                    activeUser === '' ? <h2 className='text-2xl '>Select a user </h2> :
+                        <div className='flex flex-col items-center'>
+                            <h2 className='text-2xl'>{activeUser}'s Participation Feedback:</h2>
+                            <Rating />
+                        </div>
+
+
+
+                }
 
             </div>
         </div>
