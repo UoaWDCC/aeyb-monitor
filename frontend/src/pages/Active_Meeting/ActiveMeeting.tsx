69f334fda6aadfb24593272400852e1281d48e66
Grant Liu <grant.liux@gmail.com>
2022-10-01 18:00:37 +1300

    console logging form values on submit

--- frontend/src/pages/Active_Meeting/ActiveMeeting.tsx
+++ frontend/src/pages/Active_Meeting/ActiveMeeting.tsx
@@ -4,14 +4,24 @@ import UserList from './components/UserList';
 import AttendanceSelect from './components/AttendanceSelect';
 import Rating from './components/RadioGroupRating'
 
+export enum AttendanceType {
+    NotAttended,
+    Attended
+}
 
 export default function ActiveMeeting() {
-    const allUsers = ['Hillary', 'Grant', 'Violet', 'Lauren', 'Luke', 'Sarah', 'Helen', 'Josh', 'Tyler'];
+    const allUsers = ['User1', 'Grant', 'Violet', 'Lauren', 'Luke', 'Sarah', 'Helen', 'Josh', 'Tyler'];
     const [activeUser, setActiveUser] = React.useState('');
 
 
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
+    const [attendance, setAttendance] = useState(AttendanceType.Attended);
+
+    const [rating, setRating] = useState(3);
+
 
     // toggles the sidebar being open and closed
     const toggleMenu = () => {
@@ -20,8 +30,8 @@ export default function ActiveMeeting() {
     return (
         <div className='h-screen overflow-scroll'>
             <Sidebar isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} currentPage={0} />
-            <div className='w-full sm:w-1/3 h-screen mx-auto py-2 flex items-center flex-col justify-around'>
-                <h1 className='text-4xl'>Meeting A - with Group B</h1>
+            <div className='w-5/6 sm:w-1/3 h-screen mx-auto py-2 flex items-center flex-col justify-around'>
+                <h1 className='text-5xl mt-2'>Meeting A - with Group B</h1>
 
                 <div className='w-full'>
                     <UserList allUsers={allUsers} setActiveUser={setActiveUser} />
@@ -31,12 +41,22 @@ export default function ActiveMeeting() {
                     activeUser === '' ? <h2 className='text-2xl '>Select a user </h2> :
                         <div className='flex flex-col items-center w-full'>
                             <h2 className='text-2xl my-5'>{activeUser}'s Participation Feedback:</h2>
-                            <AttendanceSelect />
-                            <Rating />
+                            <div className='my-2 w-full'>
+                                <AttendanceSelect setAttendance={setAttendance} />
+                            </div>
+                            <div className='w-full my-2 flex justify-center'>
+                                <Rating setRating={setRating} />
+                            </div>
 
-                            <textarea className=' my-5 w-full border-[#262b6c] border-2 p-2 resize-none' placeholder='Enter Comments' rows={10}></textarea>
+                            <textarea id="comment" className=' my-5 w-full border-[#262b6c] border-2 p-2 resize-none' placeholder='Enter Comments' rows={10}></textarea>
 
-                            <button className='bg-[#262b6c] text-white p-2 rounded-md'>Submit for {activeUser}</button>
+                            <button className='bg-[#262b6c] text-white p-2 rounded-md' onClick={() => (
+                                console.log(`User: ${activeUser},
+                                            Attendance: ${AttendanceType[attendance]},
+                                            Rating: ${rating},
+                                            Comments: ${(document.getElementById("comment") as HTMLInputElement).value}`)
+
+                            )}>Submit for {activeUser}</button>
                         </div>
 
 
@@ -44,6 +64,7 @@ export default function ActiveMeeting() {
                 }
 
                 <button className='mt-20 text-red-600 font-extrabold border-2 border-red-600 p-2 rounded-md w-full'>END MEETING</button>
+
 
 
             </div>
