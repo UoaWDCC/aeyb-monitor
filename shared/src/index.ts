018d37ab2fa285296bcda1206abf12c5b2322bfb
pumbas600 <joshjeffers600@gmail.com>
2022-10-02 00:14:17 +1300

    #36 Remove shared folder

--- shared/src/index.ts
+++
@@ -1,7 +0,0 @@
-// We need to export all the types we're using from this file for it to be usable in the
-// other mono-repos
-export * from './models/Test';
-
-export function hello() {
-    return 'Hello world!';
-}
--- shared/src/models/Test.ts
+++
@@ -1,4 +0,0 @@
-export interface Test {
-    name: string;
-    age: number;
-}
