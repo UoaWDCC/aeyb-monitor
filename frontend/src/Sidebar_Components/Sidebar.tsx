77e53bfa3f4540c3e3f8f7fb8e47f53f5b014de7
Tyler Donghwi Kim <98932343+TylerDonghwi@users.noreply.github.com>
2022-10-02 18:22:34 +1300

    mainlogo folder fixed

--- frontend/src/Sidebar_Components/Sidebar.tsx
+++ frontend/src/Sidebar_Components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './sidebar.css'
-import Mainlogo from './Mainlogo/Mainlogo'
+import logo from '../images/edited/AEYB_A0_Circle_resized.png'
 import Menuitem from './Menuitem/Menuitem'
 
 export default function Sidebar(prop: { isMenuOpen: boolean; toggleMenu: React.MouseEventHandler<HTMLDivElement> }) {
@@ -10,7 +10,7 @@ export default function Sidebar(prop: { isMenuOpen: boolean; toggleMenu: React.M
     return (
         <div className={prop.isMenuOpen ? "sidebar menuOpen" : "sidebar"}>
             <div className='logoBox' onClick={prop.toggleMenu}>
-                <Mainlogo />
+                <img className='relative aspect-square h-[60px] m-[15px] transition-all duration-200 active:left-[0.5px] active:top-[0.5px]' src={logo} alt="AEYB logo" />
             </div>
             <Menuitem iconIndex={0} isMenuOpen={prop.isMenuOpen} currentPage={currentPage} setCurrentPage={setCurrentPage} />
             <Menuitem iconIndex={1} isMenuOpen={prop.isMenuOpen} currentPage={currentPage} setCurrentPage={setCurrentPage} />
