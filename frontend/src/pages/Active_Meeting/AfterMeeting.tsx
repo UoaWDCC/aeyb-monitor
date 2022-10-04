83c0118a4050c1f020d39c89dfa3895b72af1964
long715 <long715@aucklanduni.ac.nz>
2022-10-05 00:10:18 +1300

    Styled After Meeting page based on UI

--- frontend/src/pages/Active_Meeting/AfterMeeting.tsx
+++ frontend/src/pages/Active_Meeting/AfterMeeting.tsx
@@ -6,7 +6,7 @@ export default function AfterMeeting() {
     const [rating, setRating] = useState(3);
     const navigate = useNavigate();
     const navToHomePage = () => {
-        navigate(".../homepage");
+        navigate("../homepage");
     };
 
     function handleSubmit() {
@@ -17,19 +17,18 @@ export default function AfterMeeting() {
 
     return (
         <div className='h-screen overflow-scroll relative'>
-            <div className='w-5/6 lg:w-1/3 h-screen mx-auto py-2 flex items-center flex-col justify-around'>
-                <h1 className='text-4xl mt-2'>Meeting A - with Group B</h1>
+            <div className='h-screen mx-auto py-2 flex items-center flex-col text-[#262B6C]'>
+                <h1 className='text-4xl mt-4 mb-10 font-bold'>Meeting A - with Group B</h1>
+                <div className='flex flex-col items-center w-5/6 lg:w-1/3 text-center'>
 
-                <div className='flex flex-col items-center w-full'>
-
-                    <h2 className='text-2xl mb-5'>Participation Feedback:</h2>
-                    <div className='w-full my-2 flex justify-center'>
+                    <h2 className='text-3xl'>How are you feeling today?</h2>
+                    <div className='w-full mt-3 mb-16 flex justify-center'>
                         <Rating setRating={setRating} />
                     </div>
+                    <h2 className='text-3xl'>How did you find the meeting?</h2>
+                    <textarea id="comment" className=' mt-4 mb-10 w-full border-[#262b6c] border-2 p-2 resize-none' placeholder='Enter Comments' rows={10}></textarea>
 
-                    <textarea id="comment" className=' my-5 w-full border-[#262b6c] border-2 p-2 resize-none' placeholder='Enter Comments' rows={10}></textarea>
-
-                    <button className='bg-[#262b6c] text-white p-2 rounded-md' onClick={() => { handleSubmit(); navToHomePage(); }}>Submit</button>
+                    <button className='text-red-600 border-red-600 border-2 p-1 rounded-md w-1/3 text-2xl' onClick={() => { handleSubmit(); navToHomePage(); }}>Submit</button>
                 </div>
             </div>
 
