c2a2e331d1f25cd9ff078058a595e08f31aa7fef
Grant Liu <grant.liux@gmail.com>
2022-10-04 19:43:33 +1300

    Added a submit notification and fixed some styling

--- frontend/src/pages/Active_Meeting/ActiveMeeting.tsx
+++ frontend/src/pages/Active_Meeting/ActiveMeeting.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import UserList from './components/UserList';
 import AttendanceSelect from './components/AttendanceSelect';
 import Rating from './components/RadioGroupRating'
+import { useNavigate } from 'react-router-dom';
 
 export enum AttendanceType {
     NotAttended,
@@ -9,7 +10,7 @@ export enum AttendanceType {
 }
 
 export default function ActiveMeeting() {
-    const allUsers = ['User1', 'Grant', 'Violet', 'Lauren', 'Luke', 'Sarah', 'Helen', 'Josh', 'Tyler'];
+    const allUsers = ['Joe', 'Bob', 'User 1', 'User 2', 'Mary', 'Jane', 'Susan'];
     const [activeUser, setActiveUser] = React.useState('');
 
     const [leaveMeetingOpen, setLeaveMeetingOpen] = useState(false);
@@ -17,6 +18,29 @@ export default function ActiveMeeting() {
     const [attendance, setAttendance] = useState(AttendanceType.Attended);
 
     const [rating, setRating] = useState(3);
+
+    const navigate = useNavigate();
+
+    const [message, setMessage] = React.useState('');
+    const DELAY = 1000;
+
+    React.useEffect(() => {
+        if (!message) { return; }
+
+        const timer = window.setTimeout(() => setMessage(''), DELAY);
+
+        return () => {
+            window.clearTimeout(timer);
+        };
+    }, [message]);
+
+    function handleSubmit() {
+        console.log(`User: ${activeUser},
+        Attendance: ${AttendanceType[attendance]},
+        Rating: ${rating},
+        Comments: ${(document.getElementById("comment") as HTMLInputElement).value}`)
+        setMessage('Feedback Submitted !!!');
+    }
 
     return (
         <div className='h-screen overflow-scroll relative'>
@@ -30,7 +54,8 @@ export default function ActiveMeeting() {
                 {
                     activeUser === '' ? <div></div> :
                         <div className='flex flex-col items-center w-full'>
-                            <h2 className='text-2xl my-5'>{activeUser}'s Participation Feedback:</h2>
+                            <h1 className='text-4xl font-bold mt-5 text-[#262b6c] '>{activeUser}'s</h1>
+                            <h2 className='text-2xl mb-5'>Participation Feedback:</h2>
                             <div className='my-2 w-full'>
                                 <AttendanceSelect setAttendance={setAttendance} />
                             </div>
@@ -40,13 +65,12 @@ export default function ActiveMeeting() {
 
                             <textarea id="comment" className=' my-5 w-full border-[#262b6c] border-2 p-2 resize-none' placeholder='Enter Comments' rows={10}></textarea>
 
-                            <button className='bg-[#262b6c] text-white p-2 rounded-md' onClick={() => (
-                                console.log(`User: ${activeUser},
-                                            Attendance: ${AttendanceType[attendance]},
-                                            Rating: ${rating},
-                                            Comments: ${(document.getElementById("comment") as HTMLInputElement).value}`)
+                            <button className='bg-[#262b6c] text-white p-2 rounded-md' onClick={handleSubmit}>Submit for {activeUser}</button>
 
-                            )}>Submit for {activeUser}</button>
+                            <div className="w-full mt-2">
+                                {message && <div className="rounded-md bg-green-100 text-green-900 p-2 text-center">{message}</div>}
+                            </div>
+
                         </div>
                 }
 
@@ -57,10 +81,10 @@ export default function ActiveMeeting() {
                     <div className='flex items-center justify-center absolute h-screen w-full top-0 left-0 '>
                         <div className='opacity-50 bg-gray-600 w-full h-full absolute top-0 left-0 z-20'></div>
                         <div className='text-5xl bg-white p-10 opacity-100 z-30 rounded-lg'>
-                            Are you sure you want to leave meeting?
+                            Are you sure you want to end the meeting?
                             <div className='flex justify-around p-5 mt-20'>
                                 <button className='bg-gray-400 p-2 rounded-md text-3xl  px-5' onClick={() => setLeaveMeetingOpen(false)}>Cancel</button>
-                                <button className=' bg-red-300 p-2 rounded-xl text-3xl px-5'>Leave</button>
+                                <button className=' bg-red-300 p-2 rounded-xl text-3xl px-5' onClick={() => navigate(`../homepage`, { replace: true })}>End Meeting</button>
 
                             </div>
                         </div>
