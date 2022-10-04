c1547e5d2dbd8aa44a01791c65c9d7d9f9d77dba
long715 <long715@aucklanduni.ac.nz>
2022-10-05 01:01:44 +1300

    Reused pop-up from Active-Meeting page for submission confirmation

--- frontend/src/pages/Active_Meeting/AfterMeeting.tsx
+++ frontend/src/pages/Active_Meeting/AfterMeeting.tsx
@@ -9,6 +9,7 @@ export default function AfterMeeting() {
         navigate("../homepage");
     };
     const [hasComment, setCommentStatus] = useState(true);
+    const [leaveMeetingOpen, setLeaveMeetingOpen] = useState(false);
 
     function handleSubmit() {
         console.log(`
@@ -18,8 +19,7 @@ export default function AfterMeeting() {
 
     function inputRequired() {
         if ((document.getElementById("comment") as HTMLInputElement).value.length != 0) {
-            handleSubmit();
-            navToHomePage();
+            setLeaveMeetingOpen(true);
         } else {
             setCommentStatus(false);
         }
@@ -42,6 +42,22 @@ export default function AfterMeeting() {
                     <button className='text-red-600 border-red-600 border-2 p-1 rounded-md w-1/3 text-2xl' onClick={inputRequired}>Submit</button>
                 </div>
             </div>
+            {
+                leaveMeetingOpen ?
+                    <div className='flex items-center justify-center absolute h-screen w-full top-0 left-0 '>
+                        <div className='opacity-50 bg-gray-600 w-full h-full absolute top-0 left-0 z-20'></div>
+                        <div className='text-5xl bg-white p-10 opacity-100 z-30 rounded-lg'>
+                            Are you sure you want to submit?
+                            <div className='flex justify-around p-5 mt-20'>
+                                <button className='bg-gray-400 p-2 rounded-md text-3xl  px-5' onClick={() => setLeaveMeetingOpen(false)}>Cancel</button>
+                                <button className=' bg-red-300 p-2 rounded-xl text-3xl px-5' onClick={() => { handleSubmit(); navToHomePage(); }}>Submit</button>
+
+                            </div>
+                        </div>
+                    </div>
+                    : <div></div>
+            }
         </div >
+
     )
 }
