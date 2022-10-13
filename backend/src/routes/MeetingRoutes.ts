342ac8f9291f2778f5e43e00e08192ef74e46bb1
Andrew Lam <bluey17501@gmail.com>
2022-10-14 11:13:50 +1300

    Added attendance routes

--- backend/src/routes/MeetingRoutes.ts
+++ backend/src/routes/MeetingRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import protect from '../middleware/AuthMiddleware';
 
-import { getAllMeetings, getMeeting, addMeeting, deleteMeeting, updateMeeting } from '../controllers/MeetingController';
+import { getAllMeetings, getMeeting, addMeeting, deleteMeeting, updateMeeting, updateAbsence, updateAttendance, addFeedback } from '../controllers/MeetingController';
 import Permission from '../shared/Types/utils/Permission';
 
 const MeetingRouter = Router();
@@ -13,5 +13,8 @@ MeetingRouter.route('/:meetingId')
     .get(protect(Permission.VIEW_MEETINGS), getMeeting)
     .delete(protect(Permission.DELETE_MEETINGS), deleteMeeting)
     .patch(protect(Permission.UPDATE_MEETINGS), updateMeeting);
+MeetingRouter.route('/:meetingId/attendance/going').post(protect(Permission.VIEW_MEETINGS), updateAbsence);
+MeetingRouter.route('/:meetingId/attendance/feedback').patch(protect(Permission.VIEW_MEETINGS), addFeedback);
+MeetingRouter.route('/:meetingId/attendance/:userId').patch(protect(Permission.ADD_MEETINGS), updateAttendance);
 
 export default MeetingRouter;
