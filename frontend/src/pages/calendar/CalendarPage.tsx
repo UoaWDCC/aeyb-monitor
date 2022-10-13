891a3cbc7a42a153d5746f42390e96c8e3f8c1f2
long715 <long715@aucklanduni.ac.nz>
2022-10-13 15:05:02 +1300

    calendar page styling of each meeting component and buttons

--- frontend/src/pages/calendar/CalendarPage.tsx
+++ frontend/src/pages/calendar/CalendarPage.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useState } from 'react'
 import WeeklyInstance from './components/WeeklyInstance'
 import { useMeetingContext } from '../../contexts/MeetingContext'
+import IonIcon from '@reacticons/ionicons';
 
 const CalendarPage = (): ReactElement => {
 
@@ -28,10 +29,14 @@ const CalendarPage = (): ReactElement => {
       <div className='pageComponent'>
         {/* CALENDAR/EVENT COLLECTION DIV */}
         <div className='flex flex-col h-full w-full md:w-1/2'>
-          <div className='flex-row text-center mt-8 mb-8'>
-            <button className=' bg-gray-200 px-4 py-4 rounded-sm w-1/8' onClick={() => { setWeekCount(weeks - 1) }}> prev </button>
-            <p className='inline-block w-1/2 text-xl font-bold text-[#5563AE]'>{previousMonth} {firstWeekDay} - {months[week.getMonth()]} {week.getDate()}</p>
-            <button className='bg-gray-200 px-4 py-4 rounded-sm w-1/8' onClick={() => setWeekCount(weeks + 1)}>next </button>
+          <div className='flex-row text-center mt-8 mb-8 text-[#262B6C]'>
+            <button className=' bg-slate-100 px-4 py-4 rounded-sm w-1/8' onClick={() => { setWeekCount(weeks - 1) }}>
+              <IonIcon name="chevron-back-outline" size='large' />
+            </button>
+            <p className='inline-block w-1/2 text-xl font-bold text-[#262B6C]'>{previousMonth} {firstWeekDay} - {months[week.getMonth()]} {week.getDate()}</p>
+            <button className='bg-slate-100 px-4 py-4 rounded-sm w-1/8' onClick={() => setWeekCount(weeks + 1)}>
+              <IonIcon name="chevron-forward-outline" size='large' />
+            </button>
           </div>
 
           {renderWeeklyMeetings()}
--- frontend/src/pages/calendar/components/WeeklyInstance.tsx
+++ frontend/src/pages/calendar/components/WeeklyInstance.tsx
@@ -1,14 +1,17 @@
-import React, { ReactElement } from 'react';
 import MeetingDTO from '../../../shared/Types/dtos/MeetingDTO';
 
 function WeeklyInstance(props: { meeting: MeetingDTO }) {
     return (
         <div>
             <div className='flex justify-center overflow-scroll'>
-                <div className={`flex-col mb-4 w-full lg:w-3/4 py-4 px-4 bg-slate-100`}>
+                <div className={`flex-col mb-4 w-full lg:w-3/4 py-4 px-4  rounded-lg text-[#262B6C] bg-slate-100`}>
                     <p className='font-bold text-3xl'>{props.meeting.name}</p>
-                    <p className='font-bold text-2xl'>{props.meeting.time}</p>
-                    <p className='text-2xl'>{props.meeting.description}</p>
+                    <div className='text-xl text-[#464a83]'>{
+                        props.meeting.description === undefined ?
+                            <p>no description.</p>
+                            : <p>{props.meeting.description}</p>
+                    }</div>
+                    <p className='text-xl mt-4'>{props.meeting.location}</p>
                 </div>
             </div>
         </div>
