359874f770bc4f45d0a3b19465dbd7e7ecf72557
long715 <long715@aucklanduni.ac.nz>
2022-10-14 13:10:16 +1300

    Roles page bug fixes

--- frontend/src/pages/roles/Roles.tsx
+++ frontend/src/pages/roles/Roles.tsx
@@ -22,25 +22,14 @@ function Roles() {
     return (
         // Page container
         <div className=" md:pl-[90px] bg-white overflow-scroll h-screen md:ml-4">
-            {/* Page heading */}
-            <div className="px-4 pt-2 flex flex-row h-[5%]">
-                {/* Return button */}
-                <div className="">
-                    <button className="text-2xl text-[#262b6c] items-center flex flex-row hover:text-[#465188]" onClick={returntoProfile} >
-                        <IonIcon name="chevron-back-outline" />
-                        back
-                    </button>
-                </div>
-
-            </div>
-            <div className=" w-full p-4 rounded-md md:grid md:grid-cols-3 md:gap-12 overflow-scroll">
+            <div className=" w-full p-4 rounded-md md:grid md:grid-cols-3 md:gap-12 overflow-scroll h-full">
                 {/* Left column of roles and users */}
-                <div className="flex flex-col">
-                    <div className="h-[40%]">
+                <div className="flex flex-col h-screen">
+                    <div className="h-[45%]">
                         <RoleList allRoles={allRoles} setActiveRole={setActiveRole} />
                     </div>
 
-                    <div className="h-[40%]">
+                    <div className="h-[45%]">
                         <UserList allUsers={allUsers} setActiveRole={setActiveRole} />
                     </div>
                 </div>
