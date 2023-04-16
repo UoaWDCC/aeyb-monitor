51de5ebbedae261ca788cb8dcf271af108454e52
jacobatan <jacob.tan229@gmail.com>
2023-04-16 13:35:55 +1200

    Delete commented code

--- shared/utils/Permission.ts
+++ shared/utils/Permission.ts
@@ -1,12 +1,3 @@
-// export const UserPermissions = [
-//     'VIEW_USERS',
-//     'MANAGE_USERS',
-//     'VIEW_MEETINGS',
-//     'MANAGE_MEETINGS',
-//     'VIEW_ROLES',
-//     'MANAGE_ROLES',
-// ] as const;
-
 export const UserPermissions = ['VIEW_USERS', 'MANAGE_USERS'] as const;
 export const MeetingPermissions = ['VIEW_MEETINGS', 'MANAGE_MEETINGS'] as const;
 export const RolePermissions = ['VIEW_ROLES', 'MANAGE_ROLES'] as const;
