fdb44ab14522c22912ef5eec1b04690f2cc5dd05
ejiang25 <ejiang25@gmail.com>
2023-04-20 15:47:01 +1200

    feat: created generalized button component

---
+++ frontend/src/utility_components/Button.tsx
@@ -0,0 +1,16 @@
+type ButtonProps = {
+	text: string;
+	onClick: () => void;
+	className: string;
+	type?: "button" | "submit" | "reset";
+};
+
+const Button = ({ text, onClick, className, type }: ButtonProps) => {
+	return (
+		<button className={className} onClick={onClick} type={type}>
+			{text}
+		</button>
+	);
+};
+
+export default Button;
