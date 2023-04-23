f9dd30a02315a526da7e14cd7b9a566d0aace0b2
tlooi <t.looi@outlook.com>
2023-04-23 12:54:38 +1200

    error prevention workaround

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import Server from './classes/Server';
 import config from './types/Config';
 import './types/ResponseAugmentation'; // Apply augmentation
+import { values } from '@shared/index';
 
 const server = new Server(config);
+console.log(values);
 
 export default server;
