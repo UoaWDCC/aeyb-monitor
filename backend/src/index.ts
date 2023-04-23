c4fc3aa2299e874bfa38d454488e1092a1707076
tlooi <t.looi@outlook.com>
2023-04-23 12:56:16 +1200

    remove testing code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,7 @@
 import Server from './classes/Server';
 import config from './types/Config';
 import './types/ResponseAugmentation'; // Apply augmentation
-import { values } from '@shared/index';
 
 const server = new Server(config);
-console.log(values);
 
 export default server;
