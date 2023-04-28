088739b1634d36ff99d530091dd17e47ea8115b2
Chris Valenzuela <dvalenzuela.chris@gmail.com>
2023-04-29 01:22:42 +1200

    requests, responses and controller were not tracked so here they are :skull:

---
+++ backend/src/controllers/LocationController.ts
@@ -0,0 +1,93 @@
+// import { Response } from 'express';
+import asyncHandler from 'express-async-handler';
+import Location from '../models/LocationModel';
+import { LocationIdParam } from '@shared/params';
+import { TypedRequest, TypedRequestParams, TypedResponse } from '../types/UtilTypes';
+import LocationDTO from '@shared/dtos/LocationDTO';
+import {
+    GetAllLocationData,
+    GetLocationData,
+    AddLocationData,
+    UpdateLocationData,
+    DeleteLocationData,
+} from '@shared/responses/LocationResponses';
+
+/**
+ * @desc 	Get all the location
+ * @route 	GET /api/locations
+ */
+const getAllLocations = asyncHandler(async (req: TypedRequest, res: TypedResponse<GetAllLocationData>) => {
+    const locations = await Location.find();
+
+    res.ok({
+        results: locations.length,
+        locations,
+    });
+});
+/**
+ * @desc 	Get a specific Location
+ * @route 	GET /api/Locations/:locationId
+ */
+const getLocation = asyncHandler(
+    async (req: TypedRequestParams<LocationIdParam>, res: TypedResponse<GetLocationData>) => {
+        const location = await Location.findById(req.params.locationId);
+        if (!location) {
+            return res.notFound(`There is no location with the id ${req.params.locationId}`);
+        }
+
+        // const userCount = await User.countDocuments({ Locations: req.params.locationId });
+
+        res.ok({
+            location,
+        });
+    },
+);
+
+/**
+ * @desc 	Add a new Location
+ * @route 	POST /api/Locations
+ */
+const addLocation = asyncHandler(async (req: TypedRequest<LocationDTO>, res: TypedResponse<AddLocationData>) => {
+    const newLocation = await Location.create(req.body);
+
+    res.created({ location: newLocation });
+});
+
+/**
+ * @desc 	Edit a specific Location
+ * @route 	PATCH /api/Locations/:locationId
+ */
+const updateLocation = asyncHandler(
+    async (req: TypedRequest<LocationDTO, LocationIdParam>, res: TypedResponse<UpdateLocationData>) => {
+        const location = await Location.findOneAndUpdate({ _id: req.params.locationId }, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        if (!location) {
+            return res.notFound(`There is no valid Location with the id ${req.params.locationId}`);
+        }
+
+        res.ok({ location });
+    },
+);
+
+/**
+ * @desc 	Delete a specific Location
+ * @route 	DELETE /api/Locations/:locationId
+ */
+const deleteLocation = asyncHandler(
+    async (req: TypedRequestParams<LocationIdParam>, res: TypedResponse<DeleteLocationData>) => {
+        // TODO: First check if no meetings have the locations before deleting
+
+        // Delete the Location
+        const response = await Location.findByIdAndDelete(req.params.locationId);
+        if (!response) {
+            return res.notFound(`There is no location with the id ${req.params.locationId}`);
+        }
+
+        res.sendStatus(501);
+    },
+);
+
+export { getAllLocations, getLocation, deleteLocation, addLocation, updateLocation };
