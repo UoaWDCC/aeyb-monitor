2881e113fbf7bc3022a11960fb3d2edcc363423d
jacobatan <jacob.tan229@gmail.com>
2023-05-08 01:18:28 +1200

    General clean up of code

--- frontend/src/pages/home/components/NewMeeting.tsx
+++ frontend/src/pages/home/components/NewMeeting.tsx
@@ -1,30 +1,46 @@
 import { faClose } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react'
-import "react-datepicker/dist/react-datepicker.css"
+import { useState } from 'react';
+import 'react-datepicker/dist/react-datepicker.css';
 import { useMeetingContext } from '../../../contexts/MeetingContext';
 import { useUserContext } from '../../../contexts/UserContext';
 import { AddMeetingRequest } from '@shared/requests/MeetingRequests';
 import { AddLocationRequest } from '@shared/requests/LocationRequests';
 import DatePickerUtil from '../../../utility_components/DatePickerUtil';
 import { addOneHour, roundToHour } from '../../../utils/timeUtil';
+import { MeetingType } from '@shared/dtos/MeetingDTO';
+import LocationDTO from '@shared/dtos/LocationDTO';
+import AttendanceDTO from '@shared/dtos/AttendanceDTO';
 
-const defaultValues: AddMeetingRequest = {
+const defaultValues: FormValuesType = {
     name: '',
+    type: 'meeting',
     location: { location: '', type: 'inPerson' },
     description: '',
-    startTime: roundToHour(new Date()).getTime(),
-    finishTime: addOneHour(roundToHour(new Date())).getTime(),
-    attendance: []
-}
+    startTime: roundToHour(new Date()),
+    finishTime: addOneHour(roundToHour(new Date())),
+    attendance: [],
+};
 
-export default function NewMeeting(props) {
+type FormValuesType = {
+    type: MeetingType;
+    name: string;
+    startTime: Date;
+    finishTime: Date;
+    location: Omit<LocationDTO, 'id'>;
+    attendance: AttendanceDTO[];
+    description?: string;
+};
 
+type NewMeetingProps = {
+    isNewMeetingOpen: boolean;
+    setIsNewMeetingOpen: (value: boolean) => void;
+};
+export default function NewMeeting({ isNewMeetingOpen, setIsNewMeetingOpen }: NewMeetingProps) {
     const userContext = useUserContext();
     const meetingContext = useMeetingContext();
 
-    const { isNewMeetingOpen, setIsNewMeetingOpen } = props
-    const [formValues, setFormValues] = useState<AddMeetingRequest>(defaultValues);
+    const [formValues, setFormValues] = useState<FormValuesType>(defaultValues);
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -33,26 +49,34 @@ export default function NewMeeting(props) {
             [name]: value,
         });
     };
-    const handleStartChange = (date) => {
+
+    const handleNestedObjectChange = (nestedObjectKey: keyof FormValuesType, { objKey, objType }) => {
+        setFormValues({
+            ...formValues,
+            [nestedObjectKey]: {
+                ...[nestedObjectKey],
+                [objKey]: objType,
+            },
+        });
+    };
+
+    const handleStartChange = (date: Date) => {
         setFormValues({
             ...formValues,
             startTime: date,
-        })
-    }
-    const handleFinishChange = (date) => {
+        });
+    };
+    const handleFinishChange = (date: Date) => {
         setFormValues({
             ...formValues,
             finishTime: date,
-        })
-    }
-
+        });
+    };
 
     function handleExit() {
-        setIsNewMeetingOpen(false)
-        setFormValues(defaultValues)
+        setIsNewMeetingOpen(false);
+        setFormValues(defaultValues);
     }
-
-
 
     async function handleSubmit(event) {
         event.preventDefault();
@@ -67,32 +91,12 @@ export default function NewMeeting(props) {
             return;
         }
 
-        const locationRequest: AddLocationRequest = {
-            location: formValues.location, type: "inPerson"
-        } satisfies AddLocationRequest
-
         const meetingRequest: AddMeetingRequest = {
+            ...formValues,
             startTime: formValues.startTime.getTime(),
             finishTime: formValues.finishTime.getTime(),
-            location: formValues.location,
-            //location: {
-            //    location: formValues.location, type: 'inPerson'
-            //},
-            description: formValues.description,
-            type: "meeting",
-            name: formValues.title,
-            /* attendance: {
-                attendedUsers: [],
-                absentUsers: new Map(),
-                invited: {
-                    userIds: [],
-                    roleIds: []
-                }
-            } */
-            attendance: []
-        } satisfies AddMeetingRequest;
+        };
 
-        console.log(formValues);
         const data = await userContext.fetcher('POST /api/meetings', meetingRequest);
         if (data) {
             meetingContext.addMeeting(data.meeting);
@@ -100,59 +104,68 @@ export default function NewMeeting(props) {
             setIsNewMeetingOpen(false);
         }
     }
+    function locationChange(e) {
+        const objKey = e.target.name;
+        const objType = e.target.value;
+        handleNestedObjectChange('location', { objKey, objType });
+    }
 
     return (
         <>
-            {
-                isNewMeetingOpen ?
-                    <div className='flex items-center justify-center fixed h-screen w-full top-0 left-0 '>
-                        <div className='opacity-50 bg-gray-600 w-full h-full absolute top-0 left-0 z-20' ></div >
-                        <div className='text-5xl bg-white p-10 opacity-100 z-30 rounded-lg w-1/2 flex flex-col items-center relative'>
-                            <button className='bg-red-400 p-2 hover:bg-red-500 rounded-md text-2xl text-white px-5 absolute top-0 right-0 mt-2 mr-2' onClick={handleExit}>
-                                <FontAwesomeIcon icon={faClose} />
+            {isNewMeetingOpen ? (
+                <div className="flex items-center justify-center fixed h-screen w-full top-0 left-0 ">
+                    <div className="opacity-50 bg-gray-600 w-full h-full absolute top-0 left-0 z-20"></div>
+                    <div className="text-5xl bg-white p-10 opacity-100 z-30 rounded-lg w-1/2 flex flex-col items-center relative">
+                        <button
+                            className="bg-red-400 p-2 hover:bg-red-500 rounded-md text-2xl text-white px-5 absolute top-0 right-0 mt-2 mr-2"
+                            onClick={handleExit}
+                        >
+                            <FontAwesomeIcon icon={faClose} />
+                        </button>
+                        <h1 className="my-5">Create new meeting</h1>
+
+                        <form className="flex flex-col items-center text-lg w-3/4" onSubmit={handleSubmit}>
+                            <input
+                                className="border-[#7d6ca3] border-2 px-1 rounded-md w-full my-2"
+                                name="name"
+                                type="text"
+                                placeholder="Meeting Name"
+                                value={formValues.name}
+                                onChange={handleInputChange}
+                                required={true}
+                            />
+                            <input
+                                className="border-[#7d6ca3] border-2 px-1 rounded-md w-full my-2"
+                                name="location"
+                                type="text"
+                                placeholder="Location"
+                                value={formValues.location.location}
+                                onChange={locationChange}
+                                required={true}
+                            />
+                            <DatePickerUtil value={formValues.startTime} handleChange={handleStartChange} />
+                            <DatePickerUtil value={formValues.finishTime} handleChange={handleFinishChange} />
+
+                            <textarea
+                                className=" my-2 w-full border-[#7d6ca3] border-2 p-2 rounded-md resize-none"
+                                name="description"
+                                placeholder="Desciption"
+                                rows={5}
+                                value={formValues.description}
+                                onChange={handleInputChange}
+                            />
+                            <button
+                                className="bg-[#7d6ca3] text-white p-2 rounded-md text-3xl  px-5 my-2"
+                                type="submit"
+                            >
+                                Submit
                             </button>
-                            <h1 className='my-5'>Create new meeting</h1>
-
-                            <form className='flex flex-col items-center text-lg w-3/4' onSubmit={handleSubmit}>
-
-                                <input
-                                    className='border-[#7d6ca3] border-2 px-1 rounded-md w-full my-2'
-                                    name="title"
-                                    type="text"
-                                    placeholder='Meeting Name'
-                                    value={formValues.title}
-                                    onChange={handleInputChange}
-                                    required={true}
-                                />
-                                <input
-                                    className='border-[#7d6ca3] border-2 px-1 rounded-md w-full my-2'
-                                    name="location"
-                                    type="text"
-                                    placeholder='Location'
-                                    value={formValues.location}
-                                    onChange={handleInputChange}
-                                    required={true}
-                                />
-
-                                <DatePickerUtil value={formValues.startTime} handleChange={handleStartChange} />
-                                <DatePickerUtil value={formValues.finishTime} handleChange={handleFinishChange} />
-
-                                <textarea
-                                    className=' my-2 w-full border-[#7d6ca3] border-2 p-2 rounded-md resize-none'
-                                    name="description"
-                                    placeholder='Desciption'
-                                    rows={5}
-                                    value={formValues.description}
-                                    onChange={handleInputChange}
-                                />
-                                <button className='bg-[#7d6ca3] text-white p-2 rounded-md text-3xl  px-5 my-2' type="submit">Submit</button>
-
-                            </form>
-                        </div>
-                    </div >
-                    : <div></div>
-            }
+                        </form>
+                    </div>
+                </div>
+            ) : (
+                <div></div>
+            )}
         </>
-    )
-
+    );
 }
