e050f612deadd0e56e12664e058c2c1b32998a9d
ejiang25 <ejiang25@gmail.com>
2023-05-09 01:38:52 +1200

    feat: monthly view of meetings

--- frontend/src/pages/calendar/CalendarPage.tsx
+++ frontend/src/pages/calendar/CalendarPage.tsx
@@ -5,23 +5,17 @@ import { useUserContext } from '../../contexts/UserContext';
 import IonIcon from '@reacticons/ionicons';
 
 const CalendarPage = (): ReactElement => {
-
-  // get the current date and the date in one week 
-  const [weeks, setWeekCount] = useState(0); // counts which week the user has naviigated to 
-  const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUNE", "JULY", "AUG", "SEP", "OCT", "NOV", "DEC"];
   const userContext = useUserContext();
-
-  const week = new Date();
-  week.setDate(week.getDate() - week.getDay() + weeks * 7);
-  const firstWeekTime = week.getTime();
-  const firstWeekDay = week.getDate();
-  const previousMonth = months[week.getMonth()];
-  week.setDate(week.getDate() + 6); // set the date to the date next week
-
   const meetingContext = useMeetingContext();
-  const renderWeeklyMeetings = () => {
+  const [month, setMonth] = useState(new Date());
+  const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUNE", "JULY", "AUG", "SEP", "OCT", "NOV", "DEC"];
+
+  const startOfMonth = new Date(month.getFullYear(), month.getMonth(), 1);
+  const endOfMonth = new Date(month.getFullYear(), month.getMonth() + 1, 0);
+
+  const renderMonthlyMeetings = () => {
     return Object.values(meetingContext.meetings)
-      .filter(meeting => (meeting.startTime <= week.getTime() && meeting.startTime >= firstWeekTime))
+      .filter(meeting => (meeting.startTime >= startOfMonth.getTime() && meeting.startTime <= endOfMonth.getTime()))
       .map(meeting => <WeeklyInstance key={meeting.id} meeting={meeting} />)
   }
 
@@ -32,15 +26,15 @@ const CalendarPage = (): ReactElement => {
         {/* CALENDAR/EVENT COLLECTION DIV */}
         <div className='flex flex-col h-full w-full md:w-1/2'>
           <div className='flex-row text-center mt-8 mb-8 text-[#262B6C]'>
-            <button className=' bg-slate-100 px-4 py-4 rounded-sm w-1/8' onClick={() => { setWeekCount(weeks - 1) }}>
+            <button className=' bg-slate-100 px-4 py-4 rounded-sm w-1/8' onClick={() => { setMonth(new Date(month.getFullYear(), month.getMonth() - 1, 1)) }}>
               <IonIcon name="chevron-back-outline" size='large' />
             </button>
-            <p className='inline-block w-1/2 text-xl font-bold text-[#262B6C]'>{previousMonth} {firstWeekDay} - {months[week.getMonth()]} {week.getDate()}</p>
-            <button className='bg-slate-100 px-4 py-4 rounded-sm w-1/8' onClick={() => setWeekCount(weeks + 1)}>
+            <p className='inline-block w-1/2 text-xl font-bold text-[#262B6C]'>{months[month.getMonth()]} {month.getFullYear()}</p>
+            <button className='bg-slate-100 px-4 py-4 rounded-sm w-1/8' onClick={() => setMonth(new Date(month.getFullYear(), month.getMonth() + 1, 1))}>
               <IonIcon name="chevron-forward-outline" size='large' />
             </button>
           </div>
-          {userContext.hasPermission('VIEW_MEETINGS') && renderWeeklyMeetings()}
+          {userContext.hasPermission('VIEW_MEETINGS') && renderMonthlyMeetings()}
         </div>
       </div>
 
