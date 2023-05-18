0bc3745605571829683a66593ba6068cd0485677
Vandan Bhatt <vandan.ubuntu@gmail.com>
2023-05-19 04:08:28 +1200

    Refactored Calendar Buttons to Generalised Buttons

--- frontend/src/pages/calendar/CalendarPage.tsx
+++ frontend/src/pages/calendar/CalendarPage.tsx
@@ -24,30 +24,6 @@ const CalendarPage = (): ReactElement => {
       .map(meeting => <WeeklyInstance key={meeting.id} meeting={meeting} />)
   }
 
-  const [leftButtonColor, setLeftButtonColor] = useState('bg-slate-300');
-  const [righButtonColor, setRightButtonColor] = useState('bg-slate-300');
-
-
-  const handleClick = (btnDirection) => {
-    setWeekCount(weeks - 1)
-
-    if (btnDirection === 'leftButton') {
-      setLeftButtonColor('bg-blue-300');
-
-      setTimeout(() => {
-        setLeftButtonColor('bg-slate-300')
-      }, 1000); // Change the color back to slate after 1000ms
-    } else {
-      setRightButtonColor('bg-blue-300');
-
-      setTimeout(() => {
-        setRightButtonColor('bg-slate-300')
-      }, 1000); // Change the color back to slate after 1000ms
-    }
-
-  };
-
-
   return (
     <>
 
@@ -62,10 +38,15 @@ const CalendarPage = (): ReactElement => {
             </Button> */}
 
             {/* Translate Left + Dim */}
-            <Button className={`${leftButtonColor} px-4 py-4 rounded-sm w-1/8 relative overflow-hidden transition duration-300 hover:-translate-x-2`} onClick={() => handleClick('leftButton')}>
+            <Button size='custom' color='#262a6c' extraStyles='px-4 py-4 rounded-sm w-20 relative overflow-hidden transition duration-300 active:-translate-x-1' onClick={() => { setWeekCount(weeks - 1) }}>
               <span className='absolute inset-0 bg-white opacity-20 transition-opacity duration-300 hover:opacity-30'></span>
               <IonIcon name="chevron-back-outline" size='large' />
             </Button>
+
+            {/* <Button className={`${leftButtonColor} px-4 py-4 rounded-sm w-1/8 relative overflow-hidden transition duration-300 hover:-translate-x-2`} onClick={() => handleClick('leftButton')}>
+              <span className='absolute inset-0 bg-white opacity-20 transition-opacity duration-300 hover:opacity-30'></span>
+              <IonIcon name="chevron-back-outline" size='large' />
+            </Button> */}
 
             <p className='inline-block w-1/2 text-xl font-bold text-[#262B6C]'>{previousMonth} {firstWeekDay} - {months[week.getMonth()]} {week.getDate()}</p>
 
@@ -75,7 +56,7 @@ const CalendarPage = (): ReactElement => {
             </Button> */}
 
             {/* Translate Right */}
-            <Button className={`${righButtonColor} px-4 py-4 rounded-sm w-1/8 relative overflow-hidden transition duration-300 hover:translate-x-2`} onClick={() => handleClick('rightButton')}>
+            <Button size='custom' color='#262a6c' extraStyles='px-4 py-4 rounded-sm w-20 relative overflow-hidden transition duration-300' onClick={() => { setWeekCount(weeks + 1) }}>
               <span className='absolute inset-0 bg-white opacity-20 transition-opacity duration-300 hover:opacity-30'></span>
               <IonIcon name="chevron-forward-outline" size='large' />
             </Button>
