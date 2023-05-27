8da623587f26cb2220b0cda783c1802321068d3a
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-05-27 23:47:18 +1200

    Added Dynamic Scroll Button Visibility

--- frontend/src/utility_components/ScrollToTop.tsx
+++ frontend/src/utility_components/ScrollToTop.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import Button from './Button';
+import { useEffect, useState } from 'react';
 import IonIcon from '@reacticons/ionicons';
 
 export default function ScrollToTop() {
+    const [ShowScrollButton, setShowScrollButton] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.pageYOffset > 400) {
+                setShowScrollButton(true);
+            } else {
+                setShowScrollButton(false);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+    }, []);
+
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
@@ -11,17 +25,19 @@ export default function ScrollToTop() {
     };
 
     return (
-        <Button
-            size="custom"
-            color="#262a6c"
-            extraStyles="fixed z-90 bottom-10 right-9 
-            md:w-16 md:h-16 md:rounded-full md:text-3xl
-            sm:w-12 sm:h-12 sm:text-xl sm:rounded-full
-            xs:w-10 xs:h-10 xs:text-lg xs:rounded-full
+        ShowScrollButton && (
+            <Button
+                size="custom"
+                color="#262a6c"
+                extraStyles="fixed z-90 bottom-10 right-9 
+            md:w-16 md:h-16 md:rounded-lg md:text-3xl
+            sm:w-12 sm:h-12 sm:text-xl sm:rounded-lg
+            xs:w-10 xs:h-10 xs:text-lg xs:rounded-lg
             drop-shadow-lg flex justify-center items-center hover:drop-shadow-2xl duration-100"
-            onClick={scrollToTop}
-        >
-            <IonIcon name="arrow-up-outline" />
-        </Button>
+                onClick={scrollToTop}
+            >
+                <IonIcon name="arrow-up-outline" />
+            </Button>
+        )
     );
 }
