6b9df9d062c202e6a2057fa14a79b520cd23f9e3
tlooi <t.looi@outlook.com>
2023-06-22 18:51:45 +1200

    initial tabs

--- frontend/src/pages/roles/Roles.tsx
+++ frontend/src/pages/roles/Roles.tsx
@@ -13,6 +13,12 @@ import { Permission } from '@shared/utils/Permission';
 import PermissionsList from './components/PermissionsList';
 import Button from 'src/utility_components/Button';
 
+const PermissionsLists: { roles: Permission[]; users: Permission[]; meetings: Permission[] } = {
+    roles: ['VIEW_ROLES', 'MANAGE_ROLES'],
+    users: ['VIEW_USERS', 'MANAGE_USERS'],
+    meetings: ['VIEW_MEETINGS', 'MANAGE_MEETINGS'],
+};
+
 function Roles() {
     const userContext = useUserContext();
     const navigate = useNavigate();
@@ -80,13 +86,13 @@ function Roles() {
     };
 
     return (
-        <div className=" md:pl-[90px] bg-white overflow-scroll h-screen md:ml-4">
-            <div className="px-4 pt-2 flex flex-row h-[5%]">
+        <div className=" md:pl-[90px] bg-white overflow-scroll md:ml-4 h-screen">
+            <div className="my-2 flex flex-row">
                 <Button size="medium" color="#262a6c" onClick={returntoProfile}>
                     <IonIcon name="chevron-back-outline" /> Back{' '}
                 </Button>
             </div>
-            <div className=" w-full p-4 rounded-md md:grid md:grid-cols-3 md:gap-12 overflow-scroll h-full">
+            {/* <div className=" w-full p-4 rounded-md md:grid md:grid-cols-3 md:gap-12 overflow-scroll h-full">
                 <div className="flex flex-col">
                     <div className="h-[40%]">
                         <RoleList
@@ -131,9 +137,143 @@ function Roles() {
                         </div>
                     )}
                 </div>
-            </div>
+            </div> */}
+            <TabManager
+                orientation='row'
+                content={[
+                    { tabTitle: 'Wowzers', tabData: ['role1', 'role2'] },
+                    { tabTitle: 'Roles' },
+                    { tabTitle: 'Users', tabData: ['user1', 'user2'] },
+                ]}
+                loader={(data) => {
+                    if (data.tabTitle === 'Wowzers') {
+                        data.tabData // readonly ['role1', 'role2']
+                        // return data.tabData.map((val, index) => {
+                        //     data.tabData // readonly ['user1', 'user2']
+                        //     return <div key={index}>{val}</div>;
+                        // });
+                        return (
+                            <TabManager 
+                                orientation='column'
+                                content={[
+                                    { tabTitle: 'Thexboss', tabData: ['Awesome', '🥚'] },
+                                    { tabTitle: 'Timothy', tabData: ['HELLO 😁'] },
+                                    { tabTitle: 'Hello World!', tabData: ['HELLO 😁', '😀😀😀😀😀'] },
+                                ]}
+                                loader={(data) => {
+                                    return data.tabData.map((val, index) => {
+                                        return <div key={index}>{val}</div>
+                                    })
+                                }}
+                            />
+                        );
+                    } else if (data.tabTitle === 'Roles') {
+                        data.tabData
+                        return <ViewRolesWindow roles={roles}  />
+                    } else if (data.tabTitle === 'Users') {
+                        data.tabData // readonly ['user1', 'user2']
+                        return <ViewUserWindow users={users} />
+                    }
+
+                    return <></>;
+                }}
+            />
         </div>
     );
 }
 
 export default Roles;
+
+function TabManager<const K, const T extends { tabTitle: string; tabData?: K }>({
+    content,
+    loader,
+    orientation = 'column',
+}: {
+    orientation?: 'column' | 'row';
+    content: T[];
+    loader: (data: T) => JSX.Element | JSX.Element[];
+}) {
+    const [activeTab, setActiveTab] = useState<number>(0);
+
+    return (
+        <div className={`w-full h-full flex break-all ${orientation === 'row' ? 'flex-col' : 'flex-row'}`}>
+            <div className={`flex border-[rgba(0, 0, 0, 0.01)] ${orientation === 'row' ? 'flex-row border-b-2' : 'flex-col border-r-2 w-[300px]'}`}>
+                {content.map((tab, index) => {
+                    return (
+                        <span
+                            key={Math.random()}
+                            onClick={() => {
+                                setActiveTab(index);
+                            }}
+
+                            className={
+                                `px-4 py-2 
+                                min-w-fit 
+                                cursor-pointer 
+                                ${index === activeTab ? 'bg-slate-300 border-slate-500' : 'hover:bg-slate-200 border-transparent'} 
+                                ${orientation === 'row' ? 'border-b-2' : 'border-r-2'}`
+                            }
+                        >
+                            {tab.tabTitle}
+                        </span>
+                    );
+                })}
+            </div>
+            <div className={`w-full h-full flex ${''}`}>
+                {activeTab !== null ? loader(content[activeTab]) : false}
+            </div>
+        </div>
+    );
+}
+
+function ViewUserWindow({users}: {users: Record<string, UserDTO>}) {
+    const content = Object.keys(users).map(username => {
+        return {
+            tabTitle: users[username].name,
+            tabData: {...users[username]}
+        }
+    });
+    
+    return (
+        <TabManager
+            content={content}
+            loader={(data) => <ViewPermissions user={data.tabData} />}
+        />
+    );
+}
+
+function ViewRolesWindow({roles}: {roles: Record<string, RoleDTO>}) {
+    const content = Object.keys(roles).map(id => {
+        return {
+            tabTitle: roles[id].name,
+            tabContent: {...roles[id]}
+        };
+    });
+    return <TabManager 
+        content={content}
+        loader={(data) => {
+            return (
+                <div className='p-6 w-full'>
+                    <h1 className='font-semibold text-2xl'>{`${data.tabContent.name} role's permissions`}</h1>
+                    {data.tabContent.permissions}
+                </div>
+            );
+        }}
+    />
+}
+
+function ViewPermissions({user}: {user: UserDTO}) {
+    return (
+        <div className='p-6 w-full'>
+            <h1 className='font-semibold text-2xl'>{user.name}'s roles</h1>
+            {user.roles.map(role => {
+                return (
+                    <div key={`${user.id}-${role.id}`}>
+                        {role.name}
+                    </div>
+                );
+            })}
+            {/* {user.roles} */}
+        </div>
+    )
+}
--- frontend/src/pages/roles/components/PermissionsList.tsx
+++ frontend/src/pages/roles/components/PermissionsList.tsx
@@ -1,15 +1,16 @@
 import Switch from '@mui/material/Switch';
 import { Permission } from '@shared/utils/Permission';
 import { useUserContext } from '../../../contexts/UserContext';
+import { createElement, useState } from 'react';
 
 //groups each permission by type
-const PermissionsLists: { roles: Permission[], users: Permission[], meetings: Permission[] } = {
-    roles: ["VIEW_ROLES", "MANAGE_ROLES"],
-    users: ["VIEW_USERS", "MANAGE_USERS"],
-    meetings: ["VIEW_MEETINGS", "MANAGE_MEETINGS"]
-}
+const PermissionsLists: { roles: Permission[]; users: Permission[]; meetings: Permission[] } = {
+    roles: ['VIEW_ROLES', 'MANAGE_ROLES'],
+    users: ['VIEW_USERS', 'MANAGE_USERS'],
+    meetings: ['VIEW_MEETINGS', 'MANAGE_MEETINGS'],
+};
 
-const allPermissions = Object.values(PermissionsLists).flat()
+const allPermissions = Object.values(PermissionsLists).flat();
 
 interface Props {
     activeRole: string;
@@ -20,86 +21,16 @@ interface Props {
 export default function PermissionsList(props: Props) {
     const userContext = useUserContext();
     //toggle all / section states
-    const allChecked = allPermissions.every(permission => props.permissions.includes(permission));
+    const allChecked = allPermissions.every((permission) => props.permissions.includes(permission));
 
     //Toggles all permissions in a section
     function toggleSection(sectionPermissions: Permission[], isChecked: boolean) {
         if (isChecked) {
             props.setPermissions([...new Set([...sectionPermissions, ...props.permissions])]);
         } else {
-            props.setPermissions(props.permissions.filter(permission => !sectionPermissions.includes(permission)));
+            props.setPermissions(props.permissions.filter((permission) => !sectionPermissions.includes(permission)));
         }
     }
-    return (
-        <>
-            <div className="flex items-center flex-row justify-between text-[#262B6C]">
-                <h1 className="text-3xl font-semibold">{props.activeRole}</h1>
-
-                {/* Select all */}
-                <div className="flex sm:justify-end pt-2">
-                    <h2 className="text-2xl">Select all</h2>
-                    <Switch
-                        checked={allChecked}
-                        onChange={(e) => props.setPermissions(e.target.checked ? allPermissions : [])}
-                        disabled={!userContext.hasPermission('MANAGE_ROLES')}
-                    />
-                </div>
-            </div>
-
-            <div className="flex flex-col overflow-scroll mt-2 ">
-                {/* Map through each section permission section */}
-                {Object.entries(PermissionsLists).map(
-                    ([key, value]) => {
-                        return (
-                            <div className="md:grid grid-cols-2 gap-2 mb-3" key={key}>
-                                {/* Section header */}
-                                <div className="col-span-2 flex text-[#262b6c]">
-                                    {/* Section nam */}
-                                    <h1 className="text-3xl">
-                                        {key.charAt(0).toUpperCase() + key.slice(1).toLowerCase()}
-                                    </h1>
-                                    {/* Toggle section switch */}
-                                    <Switch
-                                        onChange={(e) => toggleSection(value, e.target.checked)}
-                                        checked={value.every((permission) => props.permissions.includes(permission))}
-                                        disabled={!userContext.hasPermission('MANAGE_ROLES')}
-                                    />
-                                </div>
-                                {/* Map through each permission in the section */}
-                                {value.map((permission) => {
-                                    return (
-                                        <div
-                                            className="p-2 text-[#262b6c] text-2xl bg-[#bdc3e3] mt-1 flex justify-between align-bottom"
-                                            key={permission}
-                                        >
-                                            {/* Permission name */}
-                                            <p>
-                                                {(
-                                                    permission.charAt(0).toUpperCase() +
-                                                    permission.slice(1).toLowerCase()
-                                                ).replace('_', ' ')}
-                                            </p>
-
-                                            {/* Toggle individual permission switch */}
-                                            <Switch
-                                                color="secondary"
-                                                checked={props.permissions.includes(permission)}
-                                                onChange={(e) => {
-                                                    props.setPermissions(e.target.checked
-                                                        ? [...props.permissions, permission]
-                                                        : props.permissions.filter(perm => perm !== permission))
-                                                }}
-                                                disabled={!userContext.hasPermission('MANAGE_ROLES')}
-                                            />
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                        );
-                    },
-                )}
-            </div>
-        </>
-    );
+    return <>{/* <TabManager tabs={[{ tabTitle: 'Roles' }, { tabTitle: 'Permissions' }]}></TabManager> */}</>;
 }
- //
+//
