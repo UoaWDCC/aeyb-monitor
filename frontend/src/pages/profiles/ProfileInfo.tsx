c43c4ce8c8656f0d95a2ac189f7cf0665ef18aa2
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-08-12 17:25:25 +1200

    Added Dark Mode to Profile

--- frontend/src/pages/profiles/ProfileInfo.tsx
+++ frontend/src/pages/profiles/ProfileInfo.tsx
@@ -1,32 +1,44 @@
 import './UserProfile.css';
+import '../../utility_components/darkmode.css';
 import NotifBar from './components/NotificationBar';
 import SettingsBar from './components/Settings';
 import { useUserContext } from '../../contexts/UserContext';
+import { useDarkMode } from '../../utility_components/DarkModeContext';
 
 function ProfileInfo() {
-
+    const { darkMode } = useDarkMode(); // Get the dark mode state
     const userContext = useUserContext();
     const user = userContext.user;
     const roleStyle = {
-        border: `2px solid ${user.roles[0].color}`,
-        color: `${user.roles[0].color}`
-    }
+        // border: `2px solid ${user.roles[0].color}`,
+        // color: `${user.roles[0].color}`,
+        border: darkMode ? '2px solid #f8f8fe' : '2px solid #262B6C',
+        color: darkMode ? 'text-[#f8f8fe]' : 'text-[#262B6C]',
+    };
 
     return (
         <div>
-            <div className='overflow-scroll flex flex-col items-center w-screem h-screen'>
+            <div
+                className={
+                    'overflow-scroll flex flex-col items-center w-screem h-screen ' + (darkMode ? 'darkAll' : '')
+                }
+            >
                 {/*Basic Information Div
                     - one role shown which is their main role 
                     - main role should always be the first role given to the user 
                       OR group the roles into categories 
                  */}
-                <div className='w-full space-x-7 flex flex-row items-center justify-center mt-9'>
+                <div className="w-full space-x-7 flex flex-row items-center justify-center mt-9">
                     <div>
-                        <img className='rounded-full w-full' alt='profile_picture' src={user.profileUrl} />
+                        <img className="rounded-full w-full" alt="profile_picture" src={user.profileUrl} />
                     </div>
-                    <div className='text-3xl md:text-4xl'>
-                        <p className='font-bold mb-2 text-[#262B6C]'>{user.name}</p>
-                        <span className='rounded-md px-4 text-3xl' style={roleStyle}>{user.roles[0].name}</span>
+                    <div className="text-3xl md:text-4xl">
+                        <p className={'font-bold mb-2 ' + (darkMode ? 'text-[#ffffff]' : 'text-[#262B6C]')}>
+                            {user.name}
+                        </p>
+                        <span className="rounded-md px-4 text-3xl" style={roleStyle}>
+                            {user.roles[0].name}
+                        </span>
                     </div>
                 </div>
 
@@ -36,8 +48,8 @@ function ProfileInfo() {
                 <SettingsBar />
             </div>
             <NotifBar />
-        </div >
+        </div>
     );
-};
+}
 
 export default ProfileInfo;
--- frontend/src/pages/profiles/components/Settings.tsx
+++ frontend/src/pages/profiles/components/Settings.tsx
@@ -2,27 +2,40 @@ import IonIcon from '@reacticons/ionicons';
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useUserContext } from '../../../contexts/UserContext';
-
+import { useDarkMode } from '../../../utility_components/DarkModeContext';
+import '../../../utility_components/darkmode.css';
 import Roles from '../../roles/Roles';
 import Button from 'src/utility_components/Button';
 
 const SettingsBar = () => {
+    const { darkMode } = useDarkMode(); // Get the dark mode state
     const userContext = useUserContext();
     const navigate = useNavigate();
     const navToRoles = () => {
         navigate('/profilepage/roles');
     };
 
+    const lightTheme = '#262a6c';
+    const darkTheme = '#4b58c9';
+
     return (
-        <div className="flex flex-col w-3/4 md:w-3/5 mt-[5%] text-3xl text-[#262B6C]">
-            <Button size="custom" extraStyles="my-2 px-7 py-10 text-left flex-row inline-flex" color="#262b6c">
+        <div className={'flex flex-col w-3/4 md:w-3/5 mt-[5%] text-3xl'}>
+            <Button
+                size="custom"
+                extraStyles="my-2 px-7 py-10 text-left flex-row inline-flex"
+                color={darkMode ? darkTheme : lightTheme}
+            >
                 <p className="w-3/4 h-full">Personal Stats</p>
                 <div className="w-1/4 text-right relative">
                     <span className="absolute inset-y-0 right-0 w-24 bg-white opacity-10 -mr-7 -my-10"></span>
                     <IonIcon name="chevron-forward-outline" />
                 </div>
             </Button>
-            <Button size="custom" extraStyles="my-2 px-7 py-10 text-left flex-row inline-flex" color="#262b6c">
+            <Button
+                size="custom"
+                extraStyles="my-2 px-7 py-10 text-left flex-row inline-flex"
+                color={darkMode ? darkTheme : lightTheme}
+            >
                 <p className="w-3/4 h-full">Meeting Stats</p>
                 <div className="w-1/4 text-right relative">
                     <span className="absolute inset-y-0 right-0 w-24 bg-white opacity-10 -mr-7 -my-10"></span>
@@ -33,7 +46,7 @@ const SettingsBar = () => {
                 <Button
                     size="custom"
                     extraStyles="my-2 px-7 py-10 text-left flex-row inline-flex"
-                    color="#262b6c"
+                    color={darkMode ? darkTheme : lightTheme}
                     onClick={navToRoles}
                 >
                     <p className="w-3/4 h-full">Roles</p>
